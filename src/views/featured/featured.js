a15d8310c33f5a614ab737576f513a27e530f01b
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-08-10 17:41:52 +0300

    Featured view implemented

---
+++ src/views/featured/featured.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Featured() {
+  return (
+    <div>
+      <h1>Featured</h1>
+    </div>
+  );
+}
+
+export default Featured;
