4f95af9ef922391ea98ffe576f0a4923b2288d26
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-08-10 15:13:22 +0300

    Welcome view implemented

---
+++ src/views/welcome/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './welcome.css';
+
+function Welcome() {
+  return (
+    <div className='welcomeCSS'>
+      <h1>Welcome to Epic Burger</h1>
+    </div>
+  );
+}
+
+export default Welcome;
