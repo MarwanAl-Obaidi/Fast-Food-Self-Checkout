84eaba1c75ff419026092dd31ae803a9b765a45c
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-08-10 19:04:26 +0300

    Style changes for Welcome and Featured views

--- src/views/featured/featured.js
+++ src/views/featured/featured.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./featured.css";
+import NavigationBar from "../../components/navigationBar/navigationBar";
 
 function Featured() {
-  return (
-    <div>
-      <h1>Featured</h1>
-    </div>
-  );
+    return (
+        <div>
+            <NavigationBar />
+            <div className="featuredCSS">
+                <h1>Featured</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Featured;
--- src/views/welcome/welcome.js
+++ src/views/welcome/welcome.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 import './welcome.css';
 
 function Welcome() {
-  return (
-    <div className='welcomeCSS'>
-      <h1>Welcome to Epic Burger</h1>
-    </div>
-  );
+    return (
+        <div className='welcomeCSS'>
+            <h1>Welcome to Epic Burger</h1>
+            <p>Home of the best burgers in town!</p>
+            <Link to='/featured'>
+                <button className='startButton'>Get started!</button>
+            </Link>
+        </div>
+    );
 }
 
 export default Welcome;
